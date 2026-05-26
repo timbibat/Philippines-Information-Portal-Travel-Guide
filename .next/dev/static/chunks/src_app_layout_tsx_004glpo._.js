af79dda0282a826846c5423264db3f9894f63e9d
timbibat <timothybibat654@gmail.com>
2026-05-26 12:19:09 +0800

    untrack .next folder from git cached index

--- .next/dev/static/chunks/src_app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_index_0n~82ls.css","static/chunks/node_modules_next_dist_0tt2wve._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_page_tsx_0jjej_u._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_0lvvz_7._.js","static/chunks/node_modules_0sdwf1g._.js"],
-    source: "dynamic"
-});
